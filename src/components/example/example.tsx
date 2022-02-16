b2320de396cc8268082f3d1a971d33a18eb9e2e9
chenxinglong <>
2022-02-16 14:52:46 +0800

    add component example template

---
+++ src/components/example/example.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import className from 'classnames/bind';
+import style from './style.module.scss';
+
+const cx = className.bind(style);
+
+export interface ExampleProps {
+  example: string
+}
+
+const Example: React.FC<ExampleProps> = (props: ExampleProps) => {
+  const { example } = props;
+  return (
+    <div>{example}</div>
+  );
+};
+
+export default Example;
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
+import { Button } from 'antd';
 import { saveUser } from '@/utils/storageUtils';
 import routerPath from '@/router/router-path';
 
@@ -7,15 +8,15 @@ const Login: React.FC = () => {
   const history = useHistory();
   return (
     <div>
-      <button
-        type="button"
+      <Button
+        type="primary"
         onClick={() => {
           saveUser('123');
           history.push(routerPath.Home);
         }}
       >
         Login
-      </button>
+      </Button>
     </div>
   );
 };
