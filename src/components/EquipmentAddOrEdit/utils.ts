5546c19d09240b21a0c27ae56a10af6b71404422
chenxinglong <>
2022-03-22 12:17:41 +0800

    Extraction components as EquipemntAddOrEdit

---
+++ src/components/EquipmentAddOrEdit/utils.ts
@@ -0,0 +1,20 @@
+import { RcFile } from 'antd/es/upload/interface';
+
+declare const ProgressStatuses: ['exception', 'success', 'active', 'normal'];
+
+export interface IUploadType {
+  fileList: RcFile[]
+  fileBase64: string
+}
+
+export interface UploadProgressConfig {
+  status: typeof ProgressStatuses[number]
+  percent: number
+}
+
+export interface BrandTypeProps {
+  brandInfo: {
+    brand: string
+    description: string
+  }
+}
--- src/pages/EquipmentAdd/index.tsx
+++ src/pages/EquipmentAdd/index.tsx
@@ -1,37 +1,16 @@
 import React, { useRef, useState } from 'react';
 import classNames from 'classnames/bind';
-import {
-  Form, Input, Upload, Button, message, Progress,
-} from 'antd';
-import ImgCrop from 'antd-img-crop';
+import { message } from 'antd';
 import { RcFile } from 'antd/es/upload/interface';
-import { PlusOutlined } from '@ant-design/icons';
 import { useHistory } from 'react-router-dom';
 import styles from './styles.module.scss';
 import { ServicesApi } from '@/services/services-api';
 import { getBase64 } from '@/utils/fileUtils';
 import routerPath from '@/router/router-path';
+import EquipmentAddOrEdit from '@/components/EquipmentAddOrEdit';
+import { BrandTypeProps, IUploadType, UploadProgressConfig } from '@/components/EquipmentAddOrEdit/utils';
 
 const cx = classNames.bind(styles);
-
-declare const ProgressStatuses: ['exception', 'success', 'active', 'normal'];
-
-interface IUploadType {
-  fileList: RcFile[]
-  fileBase64: string
-}
-
-interface UploadProgressConfig {
-  status: typeof ProgressStatuses[number]
-  percent: number
-}
-
-interface AddBrandType {
-  brandInfo: {
-    brand: string
-    description: string
-  }
-}
 
 const { uploadAttachment, addBrand } = ServicesApi;
 
@@ -47,16 +26,6 @@ const EquipmentAdd: React.FC = () => {
   const [imgUrl, setImgUrl] = useState('');
   const [uploading, setUploading] = useState(false);
   const history = useHistory();
-
-  const layout = {
-    labelCol: { span: 4 },
-    wrapperCol: { span: 20 },
-  };
-
-  /* eslint-disable no-template-curly-in-string */
-  const validateMessages = {
-    required: '${label}不可为空',
-  };
 
   const uploadProgressEvent = (e: ProgressEvent) => {
     const percent = Math.floor((e.loaded / e.total) * 100);
@@ -82,7 +51,7 @@ const EquipmentAdd: React.FC = () => {
     });
   };
 
-  const onFinish = (values: AddBrandType) => {
+  const onFinish = (values: BrandTypeProps) => {
     const { brand, description } = values.brandInfo;
     addBrand({ description, brand, imgUrl }).then((res) => {
       message.success('添加成功');
@@ -102,55 +71,19 @@ const EquipmentAdd: React.FC = () => {
     return true;
   };
 
-  const uploadButton = (
-    <div>
-      <PlusOutlined />
-      <div style={{ marginTop: 8 }}>Upload</div>
-    </div>
-  );
-
-  // getValueFromEvent 是获取到Form.Item 绑定到的数据的值
-  // 但是Form.Item 触发时机比进行图片处理要早，所以不适用Form自带的数据处理
   return (
     <>
       <div>种类添加</div>
-      <Form {...layout} name="nest-messages" onFinish={onFinish} validateMessages={validateMessages}>
-        <Form.Item name={['brandInfo', 'brand']} label="品牌" rules={[{ required: true }]}>
-          <Input />
-        </Form.Item>
-        <Form.Item
-          label="图片"
-          extra={uploadFile.fileList[0]?.name || 'example.jpg'}
-        >
-          <div>
-            <ImgCrop rotate beforeCrop={checkFile} onModalOk={cropOkEvent}>
-              <Upload
-                listType="picture-card"
-                className="avatar-uploader"
-                showUploadList={false}
-                customRequest={uploadMethod}
-              >
-                {uploadFile.fileBase64 ? <img src={uploadFile.fileBase64} alt="avatar" style={{ width: '100%' }} /> : uploadButton}
-              </Upload>
-            </ImgCrop>
-            <div className={cx('parent')}>
-              <Progress
-                className={cx(uploading ? 'active' : 'close')}
-                percent={progressInfo?.percent}
-                status={progressInfo?.status}
-              />
-            </div>
-          </div>
-        </Form.Item>
-        <Form.Item name={['brandInfo', 'description']} label="概述">
-          <Input.TextArea />
-        </Form.Item>
-        <Form.Item wrapperCol={{ ...layout.wrapperCol, offset: 12 }}>
-          <Button type="primary" htmlType="submit" disabled={!shouldSubmit}>
-            Submit
-          </Button>
-        </Form.Item>
-      </Form>
+      <EquipmentAddOrEdit
+        checkFile={checkFile}
+        uploading={uploading}
+        uploadFile={uploadFile}
+        cropOkEvent={cropOkEvent}
+        progressInfo={progressInfo}
+        imgUrl={imgUrl}
+        shouldSubmit={shouldSubmit}
+        uploadMethod={uploadMethod}
+      />
     </>
   );
 };
